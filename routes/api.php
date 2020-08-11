401e1a9ad195964f5964e88121e4c86ba433ed97
zakky <taufiqurrohmanzq@gmail.com>
2020-08-11 15:31:05 +0100

    delete something

--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,4 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/action','MeteranController@search')->name('search');
-route::post('login','PetugasController@login');
-Route::group(['middleware'=>'auth:api'],function(){
-    Route::post('/created','MeteranController@created')->name('save');
-    Route::post('/gantiAction','GantiPasswordController@Api');
-});
-// api
 
-Route::post('petugas_password/email', 'PetugasAuth\ForgotPasswordController@sendResetLinkEmail');
-Route::get('petugas_password/reset/{token}', 'PetugasAuth\ResetPasswordController@showResetForm');
-Route::post('petugas_password/reset', 'PetugasAuth\ResetPasswordController@reset');
